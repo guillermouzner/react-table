5be481cdd5a1849bf743373739aa9671e9e7314e
guillermouzner <guillermouz16@gmail.com>
2023-06-15 01:24:52 -0300

    formatting table

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+import {FormattingTable} from "@/components/formatting-table";
 import {SimpleTable} from "@/components/simple-table";
 
 export default function HomePage() {
   return (
-    <div className="py-10 flex justify-center">
+    <div className="py-10 flex flex-col gap-5 justify-center px-5">
       <div className="text-black">
         <p>Simple Table</p>
         <SimpleTable />
+      </div>
+      <div className="text-black">
+        <p>Formatting Table</p>
+        <FormattingTable />
       </div>
     </div>
   );
---
+++ components/formatting-table.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import {ColumnDef, flexRender, getCoreRowModel, useReactTable} from "@tanstack/react-table";
+
+import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
+
+type Payment = {
+  id: string;
+  amount: number;
+  status: "pending" | "processing" | "success" | "failed";
+  email: string;
+};
+
+export const payments: Payment[] = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "489e1d42",
+    amount: 125,
+    status: "processing",
+    email: "example@gmail.com",
+  },
+  // ...
+];
+
+const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "amount",
+    header: () => <div className="text-yellow-600">Real</div>,
+    cell: ({row}) => {
+      const amount: number = row.getValue("amount");
+      const realBr = amount.toLocaleString("pt-BR", {style: "currency", currency: "BRL"});
+
+      return <div className="text-green-700">{realBr}</div>;
+    },
+  },
+];
+
+export function FormattingTable() {
+  const table = useReactTable({
+    data: payments,
+    columns: columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div className="rounded-md border text-black">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(header.column.columnDef.header, header.getContext())}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell className="h-24 text-center" colSpan={columns.length}>
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
